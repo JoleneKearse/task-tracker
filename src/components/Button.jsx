1ff9423d551b3be561c1d84809969d84bd25fa5c
Jolene Kearse <pharaohnutz@gmail.com>
2022-12-19 17:20:17 -0500

    feat: finish header & btn comps

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types"
 
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
   return (
     <button
+      onClick={onClick}
       className="btn"
       style={{ backgroundColor: color }}
     >
@@ -11,9 +12,10 @@ const Button = ({ color, text }) => {
   )
 }
 
-Button.PropTypes = {
+Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export default Button
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import Button from "./Button"
 
 const Header = () => {
+  const onClick = () => {
+    console.log(`click`)
+  }
+
   return (
     <header className="header">
       <h1>Task Tracker</h1>
       <Button
+        onClick={onClick}
         color="green"
         text="Add"
       />
