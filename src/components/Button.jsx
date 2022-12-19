9321de246e17d4bd361f08b939548ee10fe63d62
Jolene Kearse <pharaohnutz@gmail.com>
2022-12-19 16:00:58 -0500

    feat: add header & btn comp

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types"
+
+const Button = ({ color, text }) => {
+  return (
+    <button
+      className="btn"
+      style={{ backgroundColor: color }}
+    >
+      {text}
+    </button>
+  )
+}
+
+Button.PropTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+}
+
+export default Button
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import Button from "./Button"
+
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <h1>Task Tracker</h1>
+      <Button
+        color="green"
+        text="Add"
+      />
     </header>
   )
 }
