6049c88f39a9d66ea5f81eca8b02fb4f541bb13e
Jolene Kearse <pharaohnutz@gmail.com>
2022-12-20 17:44:25 -0500

    feat: task comp text, day, onDelete, fa icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Piggy Pedicure',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Grocery Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+  ])
+
+  // delete task
+  const deleteTask = (id) => {
+    console.log(`delete:`, id)
+  }
+
   return (
     <>
       <Header />
-      <Tasks />
+      <Tasks
+        tasks={tasks}
+        onDelete={deleteTask}
+      />
     </>
   )
 }
