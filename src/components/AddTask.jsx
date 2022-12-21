940113e4d7c59017e8e6efefd28379a064362bcb
Jolene Kearse <pharaohnutz@gmail.com>
2022-12-21 15:22:31 -0500

    feat: add addTask functionality

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react"
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
   const [text, setText] = useState("")
   const [day, setDay] = useState("")
   const [reminder, setReminder] = useState(false)
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+    // validation
+    if (!text) {
+      alert("Oh no! You're missing your task! 😭")
+      return
+    }
+
+    onAdd({ text, day, reminder })
+
+    // clear the form
+    setText("")
+    setDay("")
+    setReminder(false)
+  }
+
   return (
-    <form className="add-form">
+    <form
+      className="add-form"
+      onSubmit={onSubmit}
+    >
       <div className="form-control">
         <label>Task</label>
         <input
@@ -29,6 +48,7 @@ const AddTask = () => {
         <label>Set Reminder</label>
         <input
           type="checkbox"
+          checked={reminder}
           value={reminder}
           onChange={(e) => setReminder(e.currentTarget.checked)}
         />
