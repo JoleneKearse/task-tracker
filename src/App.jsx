83da4b10a9fbbebf86d544247bc551ec20ff209e
Jolene Kearse <pharaohnutz@gmail.com>
2022-12-21 17:48:43 -0500

    feat: update reminder on db server

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,16 @@ const App = () => {
     getTasks()
   }, [])
 
-  // fetch tasks
+  // fetch all tasks
   const fetchTasks = async () => {
     const res = await fetch("http://localhost:5000/tasks")
+    const data = await res.json()
+    return data
+  }
+
+  // fetch single task to toggle reminder
+  const fetchTaskForReminder = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json()
     return data
   }
@@ -49,8 +56,21 @@ const App = () => {
   }
 
   // toggle reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTaskForReminder(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
   }
 
   return (
